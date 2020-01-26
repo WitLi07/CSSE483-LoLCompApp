2a59869aba200db31ec11da67ac62cb4b1ce0f73
WitLi07 <liw4@rose-hulman.edu>
2020-01-26 13:39:50 -0500

    Finished sprint 1 features

--- app/src/main/java/edu/rose/lolcompapp/CompPageFragment.kt
+++ app/src/main/java/edu/rose/lolcompapp/CompPageFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 
 class CompPageFragment: Fragment() {
@@ -16,16 +17,33 @@ class CompPageFragment: Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_comp_page, container, false)
 
-//        view.findViewById<Button>(R.id.view_comp_btn).setOnClickListener {
-//
-//            val ft = activity?.supportFragmentManager?.beginTransaction()
-//            val fragment = CompPageFragment()
-//            ft?.replace(R.id.fragment_container, fragment)
-//            ft?.addToBackStack("team")
-//            ft?.commit()
-//        }
+        view.findViewById<Button>(R.id.add_comp_btn).setOnClickListener {
+            showAddDialog()
+
+        }
 
 
         return view
+    }
+
+    fun showAddDialog() {
+        val builder = AlertDialog.Builder(context!!)
+
+        val view = LayoutInflater.from(context!!).inflate(R.layout.add_comp_model, null, false)
+        builder.setView(view)
+
+//        titleRef.get().addOnSuccessListener { snapshot: DocumentSnapshot ->
+//            // prepopulate from firestore
+//            val author = (snapshot["title"] ?: "") as String
+//            view.edit_text.setText(author)
+//        }
+
+//        builder.setPositiveButton(android.R.string.ok) { _, _ ->
+//            val newTitle = view.edit_text.text.toString() // update local title
+////            titleRef.set(mapOf<String, String>(Pair("title", newTitle))) // update firestore
+//        }
+
+        builder.setNegativeButton(android.R.string.cancel, null)
+        builder.create().show()
     }
 }
