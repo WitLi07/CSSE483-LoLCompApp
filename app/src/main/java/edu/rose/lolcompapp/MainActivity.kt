c523e5cc3ba9a27ca8d48709138b5a8c8b63b63a
WitLi07 <liw4@rose-hulman.edu>
2020-02-03 19:15:12 -0500

    Finished setting up login page with three types of auth and logout functionality

--- app/src/main/java/edu/rose/lolcompapp/MainActivity.kt
+++ app/src/main/java/edu/rose/lolcompapp/MainActivity.kt
@@ -4,21 +4,72 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_info_page.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LoginFragment.OnLoginButtonPressedListener {
+
+    private var auth = FirebaseAuth.getInstance()
+    lateinit var authStateListener: FirebaseAuth.AuthStateListener
+    private val RC_SIGN_IN = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        initializeListeners()
+
         val ft = supportFragmentManager.beginTransaction()
         val fragment = InfoPageFragment(this)
         ft.replace(R.id.fragment_container, fragment)
         ft.commit()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        auth.addAuthStateListener(authStateListener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        auth.removeAuthStateListener(authStateListener)
+    }
+
+    fun initializeListeners() {
+        authStateListener = FirebaseAuth.AuthStateListener { auth: FirebaseAuth ->
+            val user = auth.currentUser
+
+            if (user != null) {
+                switchToInfoPage()
+//                Log.d(Constants.TAG, "UID : ${user.uid}")
+//                Log.d(Constants.TAG, "Name : ${user.displayName}")
+//                Log.d(Constants.TAG, "Email : ${user.email}")
+//                Log.d(Constants.TAG, "Phone : ${user.phoneNumber}")
+//                Log.d(Constants.TAG, "PhotoURL : ${user.photoUrl}")
+            } else {
+                switchToLoginFragment()
+            }
+        }
+    }
+
+    fun switchToInfoPage() {
+        val fragment = InfoPageFragment(this)
+        val ft = supportFragmentManager.beginTransaction()
+        ft.replace(R.id.fragment_container, fragment)
+        ft.commit()
+    }
+
+    fun switchToLoginFragment() {
+
+        val fragment = LoginFragment()
+        val ft = supportFragmentManager.beginTransaction()
+        ft.replace(R.id.fragment_container, fragment)
+        ft.commit()
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -33,7 +84,29 @@ class MainActivity : AppCompatActivity() {
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_home -> true
+            R.id.action_logout -> {
+                auth.signOut()
+                switchToLoginFragment()
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+    override fun onLoginButtonPressed() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.PhoneBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        val loginIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+
+        startActivityForResult(loginIntent, RC_SIGN_IN)
+    }
+
+
 }
